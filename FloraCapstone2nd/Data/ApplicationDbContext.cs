d569cb7301d796b07f9627e2df3a6f9d3ab5c846
Miguel Blanco Ureta <11829978@benilde.edu.ph>
2022-06-28 20:51:02 +0800

    Working Connection String

--- FloraCapstone2nd/Data/ApplicationDbContext.cs
+++ FloraCapstone2nd/Data/ApplicationDbContext.cs
@@ -1,26 +1,29 @@
 ﻿using FloraCapstone2nd.Model;
 using Microsoft.EntityFrameworkCore;
-using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
+using Pomelo.EntityFrameworkCore.MySql;
 using Microsoft.Extensions.Configuration;
-
 
 namespace FloraCapstone2nd.Data
 {
-    
+    //[DbConfigurationType(typeof(MySql.Data.EntityFramework.MySqlEFConfiguration, MySql.Data.EntityFramework))]
     public class ApplicationDbContext : DbContext
     {
-        public ApplicationDbContext(DbContextOptions options): base(options)
-        {
-            
+
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        { 
+
         }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
+            var conString = "server=localhost;database=floradb;user=root;password=root1234";
             base.OnConfiguring(optionsBuilder);
-            optionsBuilder.UseMySql("server=localhost;port=3306;database=floradb;user=root;password=root1234", ServerVersion.AutoDetect("server=localhost;port=3306;database=floradb;user=root;password=root1234"));
+            optionsBuilder.UseMySql(conString, ServerVersion.AutoDetect(conString));
         }
         //put other database tables after this
 
         public DbSet<User> Users { get; set; }
+
     }
     
     
