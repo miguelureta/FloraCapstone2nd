5326e728525173739847ef7a6b75238980b32c4b
GabrielleSingh <59948459+GabrielleSingh@users.noreply.github.com>
2022-07-08 14:51:38 +0800

    A bit of Diagnose is done na. I'm still typing out the controller though.

---
+++ FloraCapstone2nd/Controllers/DiagnoseController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FloraCapstone2nd.Controllers
+{
+    public class DiagnoseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ FloraCapstone2nd/Pages/Diagnose/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FloraCapstone2nd.Pages.Shared.Diagnose
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
